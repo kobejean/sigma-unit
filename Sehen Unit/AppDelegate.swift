d994bb070a7e528196cc805ccf88069aed86fa78
kobejean <kobejean@me.com>
2017-07-01 14:58:48 -0600

    Initial Commit

---
+++ Sehen Unit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sehen Unit
+//
+//  Created by Jean Flaherty on 7/1/17.
+//  Copyright © 2017 kobejean. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sehen Unit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sehen Unit
+//
+//  Created by Jean Flaherty on 7/1/17.
+//  Copyright © 2017 kobejean. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
