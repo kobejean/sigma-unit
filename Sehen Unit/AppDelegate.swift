5c502c762cf9919a404b9364a5608c8fe03663f6
kobejean <kobejean@me.com>
2017-07-01 18:58:07 -0600

    URL schemes working

--- Sehen Unit/AppDelegate.swift
+++ Sehen Unit/AppDelegate.swift
@@ -10,17 +10,63 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var windowController: NSWindowController!
+    
+    override init(){
+        super.init()
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        self.windowController = storyboard.instantiateController(withIdentifier: "WindowController") as! NSWindowController
+//        self.windowController.window?.toggleFullScreen(nil)
+        print("init")
+//        self.windowController.window?.makeKeyAndOrderFront(nil)
+    }
 
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    func applicationWillFinishLaunching(_ aNotification: Notification) {
+        print("applicationWillFinishLaunching")
         // Insert code here to initialize your application
+        let appleEventManager: NSAppleEventManager = NSAppleEventManager.shared()
+        appleEventManager.setEventHandler(self,
+                                          andSelector: #selector(AppDelegate.handleGetURLEvent(_:replyEvent:)),
+                                          forEventClass: AEEventClass(kInternetEventClass),
+                                          andEventID: AEEventID(kAEGetURL))
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
+    
+    func handleGetURLEvent(_ event:NSAppleEventDescriptor, replyEvent:NSAppleEventDescriptor) {
+        guard let path = event.paramDescriptor(forKeyword: keyDirectObject)!.stringValue else {
+            print("Error reading event:", event)
+            return
+        }
+        print("handleGetURLEvent")
+        
+        let url = URL(string: path)
+        let query = url?.query
+        let queryPairs = query?.components(separatedBy: "&")
+        
+        var pairs = Dictionary<String, String>()
+        for pair in queryPairs ?? [] {
+            let bits = pair.components(separatedBy: "=")
+            if bits.count != 2 { continue }
+            let key = bits[0]
+            let value = bits[1]
+            pairs[key] = value
+        }
+        print("pairs", pairs)
+        
+        guard let filepathString = pairs["filepath"] else{ return }
+        let filepath = URL(fileURLWithPath:filepathString)
+        
+        print(filepath)
+        let viewController = windowController.contentViewController as! ViewController
+        viewController.loadFile(filepath)
+        
+        self.windowController.showWindow(self)
+        self.windowController.window?.toggleFullScreen(nil)
+        
+    }
 
 }
 
--- Sehen Unit/ViewController.swift
+++ Sehen Unit/ViewController.swift
@@ -7,13 +7,21 @@
 //
 
 import Cocoa
+import AVKit
+import AVFoundation
 
 class ViewController: NSViewController {
+    @IBOutlet var playerView: AVPlayerView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        // sehen://send?filepath=/Users/kobejean/Code/Git/tf-image-kit/docs/media/kmeans_flower_1856x1392_k16_wx5.0_wy5.0.mp4
+//        let filepath = URL(fileURLWithPath: "/Users/kobejean/Code/Git/tf-image-kit/docs/media/kmeans_flower_1856x1392_k16_wx5.0_wy5.0.mp4")
+//        playerView.player = AVPlayer(url: filepath)
+//        playerView.player?.play()
     }
 
     override var representedObject: Any? {
@@ -21,7 +29,21 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+//    override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
+//        if (segue.identifier == "MainToTimer") {
+//            let viewController = segue.destinationController as! ViewController
+//            let filepath = sender as! URL
+//            viewController.filepath = filepath
+//        }
+//    }
+    
+    func loadFile(_ filepath: URL) {
+        // sehen://send?filepath=/Users/kobejean/Code/Git/tf-image-kit/docs/media/kmeans_flower_1856x1392_k16_wx5.0_wy5.0.mp4
+        print(filepath)
+        playerView.player = AVPlayer(url: filepath)
+        playerView.player?.play()
+    }
 
 }
 
