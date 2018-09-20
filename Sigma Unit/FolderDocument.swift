fee4af61f8965d10cfeff222699f065c35e71fa7
Jean Flaherty <kobejean@me.com>
2018-09-20 10:34:46 -0600

    Swift 4 migration

--- Sigma Unit/FolderDocument.swift
+++ Sigma Unit/FolderDocument.swift
@@ -25,8 +25,8 @@ class FolderDocument: NSDocument {
     
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
-        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
-        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "PictureWindowController")) as! NSWindowController
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: "PictureWindowController") as! NSWindowController
         self.addWindowController(windowController)
         viewController = windowController.contentViewController as! PictureViewController
         
--- Sigma Unit/MovieDocument.swift
+++ Sigma Unit/MovieDocument.swift
@@ -23,10 +23,10 @@ class MovieDocument: NSDocument {
     
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
-        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
-        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "MovieWindowController")) as! NSWindowController
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: "MovieWindowController") as! NSWindowController
         self.addWindowController(windowController)
-        viewController = windowController.contentViewController as! MovieViewController
+        viewController = windowController.contentViewController as? MovieViewController
 
         guard let fileURL = self.fileURL else {
             Swift.print("no file")
